7f2213a1b172e07974dfd99ac1d72a53156758c2
valentine <v@taehyung.org>
2021-01-02 19:04:09 -0500

    Create sort.py

---
+++ sort.py
@@ -0,0 +1,22 @@
+# Sifts through the file and outputs the specified string in a .txt file
+def sort(file, export, string):
+	file = open(file, 'r+')
+	for lines in file.readlines():
+		if string in lines:
+			print(lines.replace("\n", ""))
+			with open(export, "a") as output:
+				output.write(lines)
+
+# Verifies if the export file exists or not, if not create file
+def check_export(export):
+	try:
+		file = open(export, 'r')
+	except IOError:
+		file = open(export, 'w')
+
+if __name__ == "__main__":
+	file_name = input("Input the file name you want to sort: ")
+	export_file = input("Submit the export file: ")
+	check_export(export_file)
+	string = input("Insert the string that you want to sort: ")
+	sort(file_name, export_file, string)
